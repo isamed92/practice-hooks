8206a138f7ba7359bcd802e277409a7935e9469f
Isaias Medina <isamed92@gmail.com>
2021-11-01 15:59:49 -0300

    use context added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // import { CallbackHook } from './components/06-useMemo/CallbackHook';
 // import { MemoHook } from './components/06-useMemo/MemoHook';
@@ -14,5 +15,5 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { HookApp } from './HookApp';
 
-ReactDOM.render(<TodoApp/>, document.getElementById('root'));
+ReactDOM.render(<MainApp/>, document.getElementById('root'));
 
